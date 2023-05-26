fa3af328b8d0f08217ebb13142379a4577a4c1f9
MarcosAdr <marcoslobato98@gmail.com>
2023-05-25 20:22:05 -0500

    first commit

---
+++ src/main/java/com/tesis/payticket/PayTicketApplication.java
@@ -0,0 +1,13 @@
+package com.tesis.payticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayTicketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PayTicketApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/tesis/payticket/models/entity/Evento.java
@@ -0,0 +1,7 @@
+package com.tesis.payticket.models.entity;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Evento {
+}
